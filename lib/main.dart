db81d0a9139a4a5a46df9ee562985632ecba129b
Daniela06112002 <84827002+Daniela06112002@users.noreply.github.com>
2023-09-17 18:35:41 -0600

    UPDATE TAREA6

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:tarea6_200644/src/button.dart';
 
-class MyButton extends StatefulWidget {
-  @override
-  _MyButtonState createState() => _MyButtonState();
-}
-
-class _MyButtonState extends State<MyButton> {
-  String flutterImg = "";
-  int index = 0;
-  List<String> urlImg = [
-    'assets/luffy.png',
-    'assets/zoro.png',
-    'assets/nami.jpeg',
-    'assets/ussop.png',
-    'assets/sanji.png'
-  ];
-
-  void onPressButton() {
-    setState(() {
-      flutterImg = urlImg[index];
-      index = index < 4 ? index + 1 : 0;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("Stateful Widget Imagenes"),
-        ),
-        body: Container(
-          child: Center(
-              child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Image.asset(
-                urlImg[index],
-                width: 200.0,
-                height: 200.0,
-                // Puedes personalizar el ancho y alto de la imagen según tus necesidades.
-              ),
-              Padding(
-                padding: EdgeInsets.all(10.0),
-              ),
-              ElevatedButton(
-                child: Text(
-                  "Cambiar",
-                  style: TextStyle(color: Colors.white),
-                ),
-                onPressed: onPressButton,
-              )
-            ],
-          )),
-        ));
-  }
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyButton()
+    )
+  );
 }
